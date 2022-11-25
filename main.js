521c352f6dae64ff14cb64a7b6607af5ac9f4778
OsmarAlvarado <osmar.alvarado.dev@gmail.com>
2022-11-25 18:43:49 +0100

    round3

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // VARIABLES
 const smallCar = document.getElementById("smallCar");
-const sectionAdd = document.getElementById("section3");
+const sectionAdd = document.querySelector("#section3");
 const carList = document.querySelector("#carList tbody");
+console.log(carList);
 const btnEmptyCar = document.getElementById("empty-cart");
 //console.log(sectionAdd);
 
@@ -14,7 +15,7 @@ function loadEventListener() {
     // Dispara cuando se presiona agregar carrito
     sectionAdd.addEventListener("click", buyCoffe);
 
-    // Cuando se elimina un curso del carrito
+    // Cuando se elimina una compra del carrito
     smallCar.addEventListener("click", eraseCoffe);
 
     // Vaciar el carrito
@@ -26,32 +27,77 @@ function loadEventListener() {
 
 //FUNCIONES
 
-// Función que añade el curso al carrito
+// Función que añade la compra del cafe al carrito
 function buyCoffe(e) {
     e.preventDefault();
     // Delegation para enviar al carrito
-    if (e.target.classList.contains("up-car")) {
+    if (e.target.classList.contains("Add")) {
         const coffe = e.target.parentElement.parentElement;
         readDataCoffe(coffe);
     }
 }
 
-let buyStorage = []
+// Función para leer los datos del producto
+function readDataCoffe(coffe) {
+    const infCoffe = {
+        image: coffe.querySelector("img").src,
+        title: coffe.querySelector("a").textContent,
+        price: coffe.querySelector(".price").textContent,
+        id: coffe.querySelector(".Add").getAttribute("data-id")
+    }
 
-if (localStorage.getItem('notes')) {  //-- si recibe informacion del carrito
-    noteStorage = JSON.parse(localStorage.getItem('notes'))  // transforma la nota almacenada en un objeto
-} else {  // SI NO RECIBE NADA LA INPUT
-    noteStorage = []
+    insertCar(infCoffe);
 }
 
-addCoffe.addEventListener('submit', e => {
+// Muestra el curso seleccionado en el carrito
+function insertCar(coffe) {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td>
+            <img src="${coffe.image}" width="80">
+        </td>
+        <td>${coffe.title}</td>
+        <td>${coffe.price}</td>
+        <td>
+            <a href="#" class="erase-coffe" data-id="${coffe.id}">X</a>
+        </td>
+    `;
 
-    e.preventDefault() //cancela y se mantiene
-    noteStorage.push(inputNote.value) //--para que se vaya agregando notas a la lista
-    localStorage.setItem('notes', JSON.stringify(noteStorage)) // con el stringify hara que el valor se convierta en una cadena JSON
-    listBuilder(inputNote.value) //
-    inputNote.value = ''
-})
+    carList.appendChild(row);
+    saveLocalStorage(coffe);
+}
+
+// Eliminar un producto del carrito en el DOM
+function eraseCoffe(e) {
+    e.preventDefault();
+
+    let coffe;
+    let coffeId;
+
+    if (e.target.classList.contains("erase-coffe")) {
+        coffe = e.target.parentElement.parentElement;
+        e.target.parentElement.parentElement.remove();
+        coffeId = coffe.querySelector(".Add").getAttribute("data-id");
+    }
+
+    eraseCoffeLocalStorage(coffeId);
+}
+
+// Vaciar el carrito de compras
+function emptyCart(e) {
+    e.preventDefault
+
+    // Forma lenta
+    //carList.innerHTML = "";
+
+    // Forma mas rapida y recomendada
+    while (carList.firstChild) {
+        carList.removeChild(carList.firstChild);
+    }
+
+    localStorage.removeItem("sectionAdd");
+    return false;
+}
 
 
 
