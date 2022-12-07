9635a275125b32a7ab4e7d665a11549b4976f5e2
OsmarAlvarado <osmar.alvarado.dev@gmail.com>
2022-12-07 21:13:56 +0100

    hecho

--- main.js
+++ main.js
@@ -1,39 +1,33 @@
 // VARIABLES
-const smallCar = document.getElementById("smallCar");
-const coffes = document.querySelector("#bags");//.products
-const carList = document.querySelector("#carList tbody"); //card-items
+
+const allCoffes = document.querySelector("#bags");//.products
+
+const carList = document.querySelector(".card-items"); //card-items
 const priceTotal = document.querySelector('.price-total')
 const amountProduct = document.querySelector('.count-product');
 const btnEmptyCar = document.getElementById("empty-cart");
-//console.log(sectionAdd);
+console.log();
 
 let buyCoffe = [];
 let totalCoffes = 0;
-let countProduct = 0;
-
+let countCoffes = 0;
 
 //--HACER EL LOCAL STORAGE
 
-loadEventListener();
+loadEventListenrs()
 
-function loadEventListener() {
-    // Dispara cuando se presiona agregar carrito
-    coffes.addEventListener("click", addCoffe);
+function loadEventListenrs() {
+    //agregar carrito
+    allCoffes.addEventListener('click', addCoffes);
 
     // Cuando se elimina una compra del carrito
-    carList.addEventListener("click", eraseCoffe);
+    carList.addEventListener('click', deleteCoffes);
 
     // Vaciar el carrito
     btnEmptyCar.addEventListener("click", emptyCart);
-
-    // AL cargar el documento, mostrar el localStorage
-    document.addEventListener("DOMContentLoaded", loadLocalStorage);
 }
 
-//FUNCIONES
-
-// Función que añade la compra del cafe al carrito
-function addCoffe(e) {
+function addCoffes(e) {
     e.preventDefault();
     // Delegation para enviar al carrito
     if (e.target.classList.contains("Add")) {
@@ -42,102 +36,87 @@ function addCoffe(e) {
     }
 }
 
-// Eliminar un producto del carrito en el DOM
-function eraseCoffe(e) {
-    e.preventDefault();
-
-    let coffe;
-
-    if (e.target.classList.contains("erase-coffe")) {
-
-        coffe = e.target.parentElement.parentElement;
-        e.target.parentElement.parentElement.remove();
-
-        const coffeId = e.target.getAttribute("data-id");
+function deleteCoffes(e) {
+    if (e.target.classList.contains('delete-product')) {
+        const deleteId = e.target.getAttribute('data-id');
 
         buyCoffe.forEach(value => {
-            if (value.id == coffeId) {
-                let reduce = parseFloat(value.price) * parseFloat(value.amount)
-                totalBags = totalBags - reduce
-                totalBags = totalBags.toFixed(2);
+            if (value.id === deleteId) {
+                let priceReduce = parseFloat(value.price) * parseFloat(value.amount);
+                totalCoffes = totalCoffes - priceReduce;
+                totalCoffes = totalCoffes.toFixed(2);
             }
         });
-        buyCoffe = buyCoffe.filter(coffe => coffe.id !== coffeId);
+        buyCoffe = buyCoffe.filter(product => product.id !== deleteId);
 
-        countProduct--;
+        countCoffes--;
     }
-
     //FIX: El contador se quedaba con "1" aunque ubiera 0 productos
     if (buyCoffe.length === 0) {
         priceTotal.innerHTML = 0;
         amountProduct.innerHTML = 0;
     }
-    insertCar(infCoffe);
+    loadHtml();
 }
 
-
-
-// Función para leer los datos del producto
-function readDataCoffe(coffe) {
-    const infCoffe = {
-        image: coffe.querySelector("img").src,
-        title: coffe.querySelector(".country").textContent,
-        price: coffe.querySelector(".price").textContent,
-        id: coffe.querySelector("a").getAttribute("data-id"),
+function readDataCoffe(product) {
+    const infoCoffes = {
+        image: product.querySelector('img').src,
+        title: product.querySelector('.country').textContent,
+        price: product.querySelector('.price').textContent,
+        id: product.querySelector('.Add').getAttribute('data-id'),
         amount: 1
     }
 
-    totalCoffes = parseFloat(totalCoffes) + parseFloat(infCoffe.price);
-    totalCoffes = totalCoffes.toFixed(2)
+    totalCoffes = parseFloat(totalCoffes) + parseFloat(infoCoffes.price);
+    totalCoffes = totalCoffes.toFixed(2);
 
-    const exist = buyCoffe.some(coffe => coffe.id === infCoffe.id);
+    const exist = buyCoffe.some(product => product.id === infoCoffes.id);
     if (exist) {
-        const prodCoffe = buyCoffe.map(coffe => {
-            if (coffe.id === infCoffe.id) {
-                coffe.amount++;
-                return coffe;
+        const pro = buyCoffe.map(product => {
+            if (product.id === infoCoffes.id) {
+                product.amount++;
+                return product;
             } else {
-                return coffe
+                return product
             }
-        })
-        buyCoffe = [...prodCoffe];
-
+        });
+        buyCoffe = [...pro];
     } else {
-        buyCoffe = [...buyCoffe, infCoffe]
-        countProduct++;
+        buyCoffe = [...buyCoffe, infoCoffes]
+        countCoffes++;
     }
+    loadHtml();
 
-    insertCar(infCoffe);
 }
 
-// Muestra el producto seleccionado en el carrito
-function insertCar(coffe) {
-    //clearHtml();
-    const row = document.createElement("tr");
-    row.innerHTML = `
-        <td>
-            <img src="${coffe.image}" width="80">
-        </td>
-        <td>${coffe.title}</td>
-        <td>${coffe.price}</td>
-        <td> Amount:${coffe.amount}</td>
-        <td>
-            <a href="#" class="erase-coffe" data-id="${coffe.id}">X</a>
-        </td>
-    `;
+function loadHtml() {
+    clearHtml();
+    buyCoffe.forEach(product => {
+        const { image, title, price, amount, id } = product;
+        const row = document.createElement('div');
+        row.classList.add('item');
+        row.innerHTML = `
+            <img src="${image}" width="60px"alt="">
+            <div class="item-content">
+                <h5>${title}</h5>
+                <h5 class="cart-price">${price}</h5>
+                <h6>Amount: ${amount}</h6>
+            </div>
+            <span class="delete-product" data-id="${id}">X</span>
+        `;
 
-    carList.appendChild(row);
-    priceTotal.innerHTML = totalCoffes;
-    amountProduct.innerHTML = countProduct;
-    saveLocalStorage(coffe);
+        carList.appendChild(row);
+
+        priceTotal.innerHTML = totalCoffes;
+
+        amountProduct.innerHTML = countCoffes;
+    });
+}
+function clearHtml() {
+    carList.innerHTML = '';
 }
 
-// function clearHtml() {
-//     carList.innerHTML = '';
-// }
-
-
-// Vaciar el carrito de compras
 function emptyCart(e) {
     e.preventDefault
 
@@ -149,67 +128,212 @@ function emptyCart(e) {
         carList.removeChild(carList.firstChild);
     }
 
-    localStorage.removeItem("coffes");
+
+    priceTotal.innerHTML = 0;
+    amountProduct.innerHTML = 0;
+    localStorage.removeItem("product");
+
     return false;
 }
 
-// Almacena productos en el carrito al localStorage
-function saveLocalStorage(coffe) {
-    // Tomar el valor del localStorage
-    let coffes = getCoffesLocalStorage();
-    // El producto seleccionado se agrega al arreglo
-    coffes.push(coffe);
-    localStorage.setItem("coffes", JSON.stringify(coffes));
 
-}
 
-// Obtener los productos al localStorage
-function getCoffesLocalStorage() {
-    let coffesLS;
 
-    // Comprobamos si hay algo el en localStorage
-    if (localStorage.getItem("coffes") === null) {
-        coffesLS = [];
-    } else {
-        coffesLS = JSON.parse(localStorage.getItem("coffes"));
-    }
 
-    return coffesLS;
-}
+// function loadEventListener() {
+//     //agregar carrito
+//     coffes.addEventListener("click", addCoffe);
 
-// Funcion que imprime los productos del localStorage en el carrito
-function loadLocalStorage() {
-    let coffesLS = getCoffesLocalStorage();
-    coffesLS.forEach(coffe => {
-        const row = document.createElement("tr");
-        row.innerHTML = `
-            <td>
-                <img src="${coffe.image}" width="80">
-            </td>
-            <td>${coffe.title}</td>
-            <td>${coffe.price}</td>
-            <td> Amount:${coffe.amount}</td>
-            <td>
-                <a href="#" class="erase-coffe" data-id="${coffe.id}">X</a>
-            </td>
-        `;
+//     // Cuando se elimina una compra del carrito
+//     carList.addEventListener("click", eraseCoffe);
 
-        carList.appendChild(row);
-    });
-}
+//     // Vaciar el carrito
+//     btnEmptyCar.addEventListener("click", emptyCart);
 
-// Elimina el producto por el ID del localStorage
-function eraseCoffeLocalStorage(coffeId) {
-    let coffesLS;
-    coffesLS = getCoffesLocalStorage();
-    coffesLS.forEach((coffe, index) => {
-        if (coffeId === coffe.id) {
-            coffesLS.splice(index, 1);
-        }
-    });
+//     // AL cargar el documento, mostrar el localStorage
+//     document.addEventListener("DOMContentLoaded", loadLocalStorage);
+// }
 
-    localStorage.setItem("coffes", JSON.stringify(coffesLS));
-}
+// //FUNCIONES
+
+// // Función que añade la compra del cafe al carrito
+// function addCoffe(e) {
+//     e.preventDefault();
+//     // Delegation para enviar al carrito
+//     if (e.target.classList.contains("Add")) {
+//         const coffe = e.target.parentElement;
+//         readDataCoffe(coffe);
+//     }
+// }
+
+// // Eliminar un producto del carrito en el DOM
+// function eraseCoffe(e) {
+//     e.preventDefault();
+
+//     let coffe;
+
+//     if (e.target.classList.contains("erase-coffe")) {
+
+//         coffe = e.target.parentElement.parentElement;
+//         e.target.parentElement.parentElement.remove();
+
+//         const coffeId = e.target.getAttribute("data-id");
+
+//         buyCoffe.forEach(value => {
+//             if (value.id == coffeId) {
+//                 let reduce = parseFloat(value.price) * parseFloat(value.amount)
+//                 totalBags = totalBags - reduce
+//                 totalBags = totalBags.toFixed(2);
+//             }
+//         });
+//         buyCoffe = buyCoffe.filter(coffe => coffe.id !== coffeId);
+
+//         countProduct--;
+//     }
+
+//     //FIX: El contador se quedaba con "1" aunque ubiera 0 productos
+//     if (buyCoffe.length === 0) {
+//         priceTotal.innerHTML = 0;
+//         amountProduct.innerHTML = 0;
+//     }
+//     insertCar(infCoffe);
+// }
+
+
+
+// // Función para leer los datos del producto
+// function readDataCoffe(coffe) {
+//     const infCoffe = {
+//         image: coffe.querySelector("img").src,
+//         title: coffe.querySelector(".country").textContent,
+//         price: coffe.querySelector(".price").textContent,
+//         id: coffe.querySelector("a").getAttribute("data-id"),
+//         amount: 1
+//     }
+
+//     totalCoffes = parseFloat(totalCoffes) + parseFloat(infCoffe.price);
+//     totalCoffes = totalCoffes.toFixed(2)
+
+//     const exist = buyCoffe.some(coffe => coffe.id === infCoffe.id);
+//     if (exist) {
+//         const prodCoffe = buyCoffe.map(coffe => {
+//             if (coffe.id === infCoffe.id) {
+//                 coffe.amount++;
+//                 return coffe;
+//             } else {
+//                 return coffe
+//             }
+//         })
+//         buyCoffe = [...prodCoffe];
+
+//     } else {
+//         buyCoffe = [...buyCoffe, infCoffe]
+//         countProduct++;
+//     }
+
+//     insertCar(infCoffe);
+// }
+
+// // Muestra el producto seleccionado en el carrito
+// function insertCar(coffe) {
+//     //clearHtml();
+//     const row = document.createElement("tr");
+//     row.innerHTML = `
+//         <td>
+//             <img src="${coffe.image}" width="80">
+//         </td>
+//         <td>${coffe.title}</td>
+//         <td>${coffe.price}</td>
+//         <td> Amount:${coffe.amount}</td>
+//         <td>
+//             <a href="#" class="erase-coffe" data-id="${coffe.id}">X</a>
+//         </td>
+//     `;
+
+//     carList.appendChild(row);
+//     priceTotal.innerHTML = totalCoffes;
+//     amountProduct.innerHTML = countProduct;
+//     saveLocalStorage(coffe);
+// }
+
+// // function clearHtml() {
+// //     carList.innerHTML = '';
+// // }
+
+
+// // Vaciar el carrito de compras
+// function emptyCart(e) {
+//     e.preventDefault
+
+//     // Forma lenta
+//     //carList.innerHTML = "";
+
+//     // Forma mas rapida y recomendada
+//     while (carList.firstChild) {
+//         carList.removeChild(carList.firstChild);
+//     }
+
+//     localStorage.removeItem("coffes");
+//     return false;
+// }
+
+// // Almacena productos en el carrito al localStorage
+// function saveLocalStorage(coffe) {
+//     // Tomar el valor del localStorage
+//     let coffes = getCoffesLocalStorage();
+//     // El producto seleccionado se agrega al arreglo
+//     coffes.push(coffe);
+//     localStorage.setItem("coffes", JSON.stringify(coffes));
+
+// }
+
+// // Obtener los productos al localStorage
+// function getCoffesLocalStorage() {
+//     let coffesLS;
+
+//     // Comprobamos si hay algo el en localStorage
+//     if (localStorage.getItem("coffes") === null) {
+//         coffesLS = [];
+//     } else {
+//         coffesLS = JSON.parse(localStorage.getItem("coffes"));
+//     }
+
+//     return coffesLS;
+// }
+
+// // Funcion que imprime los productos del localStorage en el carrito
+// function loadLocalStorage() {
+//     let coffesLS = getCoffesLocalStorage();
+//     coffesLS.forEach(coffe => {
+//         const row = document.createElement("tr");
+//         row.innerHTML = `
+//             <td>
+//                 <img src="${coffe.image}" width="80">
+//             </td>
+//             <td>${coffe.title}</td>
+//             <td>${coffe.price}</td>
+//             <td> Amount:${coffe.amount}</td>
+//             <td>
+//                 <a href="#" class="erase-coffe" data-id="${coffe.id}">X</a>
+//             </td>
+//         `;
+
+//         carList.appendChild(row);
+//     });
+// }
+
+// // Elimina el producto por el ID del localStorage
+// function eraseCoffeLocalStorage(coffeId) {
+//     let coffesLS;
+//     coffesLS = getCoffesLocalStorage();
+//     coffesLS.forEach((coffe, index) => {
+//         if (coffeId === coffe.id) {
+//             coffesLS.splice(index, 1);
+//         }
+//     });
+
+//     localStorage.setItem("coffes", JSON.stringify(coffesLS));
+// }
 
 //VAMOS CON EL FORMULARIO
 
@@ -240,7 +364,7 @@ const formValidation = (e) => {
         return false;
     }
 
-    if (!inputEmail.value.includes('@') || !inputEmail.value.includes('.')) {
+    if (!inputEmail.value.includes('@')) {
         alert('ERROR: colocar el @')
         inputEmail.focus()
         return
@@ -251,13 +375,6 @@ const formValidation = (e) => {
         inputEmail.focus()
         return
     }
-
-
-    // if (!(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(inputEmail.value))) {
-    //     alert('ERROR, colocar de forma correcta el email')
-    //     inputEmail.focus();
-    //     return false;
-    // }
 
     const inputPhone = document.querySelector('.phonebox input')
     if (inputPhone.value === "") {
@@ -281,7 +398,7 @@ const formValidation = (e) => {
 
     const inputCheckBox = document.querySelector('.checkBoxForm input');
     if (!inputCheckBox.checked) {
-        alert('EEROR, aceptar haber leido nuestras politicas y términos');
+        alert('[ERROR], aceptar haber leido nuestras politicas y términos');
         inputCheckBox.focus();
         return false;
     }
